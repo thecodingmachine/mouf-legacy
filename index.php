1a3f8132dba62a0be4a74806ca49ac33978c7a37
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2009-08-28 08:16:03 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@7 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		
 		<p>Apparently, this is the first time you are running Mouf. You will need to install it.</p>
 		<p>Please click the install button below. This will create and install a ".htaccess" file in the "Mouf" directory.
-		This will also create 4 files in your root directory: Mouf.php, MoufComponents.php, MoufRequire.php and MoufUniversalParameters.php</p>
+		This will also create 4 files in your root directory: Mouf.php, MoufComponents.php, MoufRequire.php and MoufUniversalParameters.php (if they don't already exist)</p>
 		<p>Please make sure that the Mouf directory is writable by your web-server.</p>
 		
 		<form action="install.php" method="post">
