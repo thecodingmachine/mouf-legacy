1f346b2a0e111ce08f94bbda127e50661da642b9
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2009-09-04 10:05:28 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@32 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ views/packages/displayConfirmPackagesEnable.php
@@ -0,0 +1,61 @@
+<h1>Packages List</h1>
+<h2>You selected this package:</h2>
+
+<?php 
+	echo "<div class='group'>Group: <b>".htmlentities($this->package->getDescriptor()->getGroup())."</b></div>";
+	echo "<div class='outerpackage'>";
+	echo "<div class='package'><span class='packagename'>".htmlentities($this->package->getDisplayName())."</span> <span class='packgeversion'>(version ".htmlentities($this->package->getDescriptor()->getVersion()).")</span>";
+	if ($this->package->getShortDescription() || $this->package->getDocUrl()) {
+		echo "<div class='packagedescription'>";
+		echo $this->package->getShortDescription();
+		if ($this->package->getShortDescription() && $this->package->getDocUrl()) {
+			echo "<br/>";
+		}
+		if ($this->package->getDocUrl()) {
+			echo "Documentation URL: <a href='".htmlentities($this->package->getDocUrl())."'>".$this->package->getDocUrl()."</a>";
+		}
+		echo "</div>";
+	}	
+	echo "</div></div>";
+?>
+
+<h2>The following packages needs to be enabled too:</h2>
+
+<div id="packageList">
+<?php 
+$oldGroup = "";
+foreach ($this->moufDependencies as $package) {
+	if ($package->getDescriptor()->getGroup() != $oldGroup) {
+		echo "<div class='group'>Group: <b>".htmlentities($package->getDescriptor()->getGroup())."</b></div>";
+		$oldGroup = $package->getDescriptor()->getGroup();
+	}
+	echo "<div class='outerpackage'>";
+	echo "<div class='package'><span class='packagename'>".htmlentities($package->getDisplayName())."</span> <span class='packgeversion'>(version ".htmlentities($package->getDescriptor()->getVersion()).")</span>";
+	if ($package->getShortDescription() || $package->getDocUrl()) {
+		echo "<div class='packagedescription'>";
+		echo $package->getShortDescription();
+		if ($package->getShortDescription() && $package->getDocUrl()) {
+			echo "<br/>";
+		}
+		if ($package->getDocUrl()) {
+			echo "Documentation URL: <a href='".htmlentities($package->getDocUrl())."'>".$package->getDocUrl()."</a>";
+		}
+		echo "</div>";
+	}
+	
+	echo "</div></div>";
+}
+$packageXmlPath = $this->package->getDescriptor()->getPackageXmlPath();
+echo "<form action='enablePackage' method='POST'>";
+echo "<input type='hidden' name='selfedit' value='".$this->selfedit."' />";
+echo "<input type='hidden' name='name' value='".htmlentities($packageXmlPath)."' />";
+echo "<input type='hidden' name='confirm' value='true' />";
+echo "<button>Enable all listed packages</button>";
+echo "</form>";
+?>
+
+
+
+</div>
+<br/>
+<br/>
