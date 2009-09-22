95dc5f0b8a26d617e5edda12b0b8d7f4ebfab01e
michael_durand <michael_durand@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2009-09-22 15:41:39 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@67 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ utils/common/smart_dump/1.0/smart_dump.php
@@ -0,0 +1,51 @@
+<?php
+
+function smart_dump_recurs($variable) {
+    if ($variable === true) {
+        return 'true';
+    } else if ($variable === false) {
+        return 'false';
+    } else if ($variable === null) {
+        return 'null';
+    } else if (is_array($variable)) {
+        $html = "<table class='dump' cellspacing='0' cellpadding='0'>\n";
+        $html .= "<thead><tr><td><b>KEY</b></td><td><b>VALUE</b></td></tr></thead>\n";
+        $html .= "<tbody>\n";
+        foreach ($variable as $key => $value) {
+            $value = smart_dump_recurs($value);
+            $html .= "<tr><td class='key'>$key</td><td class='value'>$value</td></tr>\n";
+        }
+        $html .= "</tbody>\n";
+        $html .= "</table>";
+        return $html;
+    } else {
+        return strval($variable);
+    }
+}
+
+/**
+ * Displays a variable with a smart layout in HTML
+ *
+ * @param mixed $variable
+ * @return string
+ */
+function smart_dump($variable) {
+	$dump = smart_dump_recurs($variable);
+	$script="
+	<style>
+<!--
+table.dump {
+	border:2px solid #8aF;
+}
+table.dump tr td.key {
+	border:1px solid #eee;font-weight:bold
+}	
+table.dump tr td.value {
+	border:1px solid #eee;color:#3a3
+}	
+-->
+</style>";
+	
+	return $script.$dump;
+}
+?>
