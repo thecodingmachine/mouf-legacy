0fa47f7f1374fcd34813902313a8fde4d0a12a25
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2009-10-20 16:01:42 +0000

    Modified the adswizz stat importer to have detailed logs and to round the date the stats are requested at.
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@112 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ utils/log/output_logger/1.0/OutputLogger.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * A logger class that writes messages directly to the screen (in the HTML if we are in a webpage, in the output if we are on the command line).
+ * Therefore, the output is displayed in the HTML if we are in a webpage, in the output if we are on the command line.
+ * This is very useful in development. For production servers, please prefer a logger that writes in a file (like the ErrorLogLogger).
+ * 
+ * @Component
+ */
+class OutputLogger implements LogInterface {
+	
+	private static $TRACE = 1;
+	private static $DEBUG = 2;
+	private static $INFO = 3;
+	private static $WARN = 4;
+	private static $ERROR = 5;
+	private static $FATAL = 6;
+	
+	/**
+	 * The minimum level that will be tracked by this logger.
+	 * Any log with a level below this level will not be logger.
+	 *
+	 * @Property
+	 * @Compulsory 
+	 * @OneOf "1","2","3","4","5","6"
+	 * @OneOfText "TRACE","DEBUG","INFO","WARN","ERROR","FATAL"
+	 * @var int
+	 */
+	public $level;
+	
+	public function trace($string, Exception $e=null) {
+		if($this->level<=self::$TRACE) {
+			self::logMessage("TRACE", $string, $e);
+		}
+	}
+	public function debug($string, Exception $e=null) {
+		if($this->level<=self::$DEBUG) {
+			self::logMessage("DEBUG", $string, $e);
+		}
+	}
+	public function info($string, Exception $e=null) {
+		if($this->level<=self::$INFO) {
+			self::logMessage("INFO", $string, $e);
+		}
+	}
+	public function warn($string, Exception $e=null) {
+		if($this->level<=self::$WARN) {
+			self::logMessage("WARN", $string, $e);
+		}
+	}
+	public function error($string, Exception $e=null) {
+		if($this->level<=self::$ERROR) {
+			self::logMessage("ERROR", $string, $e);
+		}
+	}
+	public function fatal($string, Exception $e=null) {
+		if($this->level<=self::$FATAL) {
+			self::logMessage("FATAL", $string, $e);
+		}
+	}
+
+	private static function logMessage($level, $string, $e=null) {
+		if ($e == null) {
+			if (!$string instanceof Exception) {
+				$trace = debug_backtrace();
+				echo($level.': '.$trace[1]['file']."(".$trace[1]['line'].") ".$trace[2]['class'].$trace[2]['type'].$trace[2]['function']." -> ".$string."\n");
+			} else {
+				echo($level.': '.ExceptionUtils::getTextForException($string)."\n");
+			}
+		} else {
+			$trace = debug_backtrace();
+			echo($level.': '.$trace[1]['file']."(".$trace[1]['line'].") ".$trace[2]['class'].$trace[2]['type'].$trace[2]['function']." -> ".$string."\n".ExceptionUtils::getTextForException($e)."\n");
+		}
+
+	}
+}
+
+?>
