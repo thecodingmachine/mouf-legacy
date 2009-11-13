fbb997063d289cb93c6304fd0394e110664bc696
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2009-11-13 16:43:11 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@151 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- html/widgets/basewidget/1.0/views/enableDisableEdition.php
+++ html/widgets/basewidget/1.0/views/enableDisableEdition.php
@@ -1,4 +1,4 @@
-<h1>Message edition</h1>
+<h1>Widget edition</h1>
 <p>When navigating in your application, you can have links automatically added by Mouf to edit your widgets. By enabling 
 the widget edition feature, you will be able to edit widgets directly into you web application.</p>
 <p>Widget edition is <b><?php echo ($this->isWidgetEditionMode==true)?"enabled":"disabled" ?></b></p>
