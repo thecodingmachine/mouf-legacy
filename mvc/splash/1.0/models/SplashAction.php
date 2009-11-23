47152ceeb466f8a8f76641e179b4dd186edd681c
michael_durand <michael_durand@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2009-11-23 17:42:28 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@162 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ mvc/splash/1.0/models/SplashAction.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Splash Action ...
+ * 
+ * @Component
+ */
+class SplashAction {
+	/**
+	 * Enter description here...
+	 *
+	 * @Property
+	 * @Compulsory
+	 * @var Controller
+	 */
+	public $controller;
+	/**
+	 * Enter description here...
+	 *
+	 * @Property
+	 * @var string
+	 */
+	public $actionName;
+}
+?>
