97ceba7910d9b40e1660f019145a314c1e03e735
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2009-11-24 17:18:43 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@168 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ database/formatters/1.0/DateFormatter.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The date formatter is used to format a date in a column.
+ * It must be attached to a column in order to be activated.
+ *
+ * @Component
+ */
+class DateFormatter implements FormatterInterface {
+
+	/**
+	 * The format of the source. The format uses the PHP notation (for instance: "Y-m-d").
+	 * You can also enter the special value "timestamp" if the source is a timestamp.
+	 * 
+	 * @Property
+	 * @Compulsory
+	 * @var string
+	 */
+	public $sourceFormat;
+	
+	/**
+	 * The format of the destination. The format uses the PHP notation (for instance: "Y-m-d").
+	 * Please note that if the $useI18nForDest variable is set to true, then instead of specifying
+	 * the format, you must put the key to a label that contains the format.
+	 * This is useful to correctly render the date depending on the locale. 
+	 * 
+	 * @Property
+	 * @Compulsory
+	 * @var string
+	 */
+	public $destFormat;
+	
+	/**
+	 * If set to true, the $destFormat property must contain a key to a label that contains the format.
+	 * This is useful to correctly render the date depending on the locale. 
+	 * 
+	 * @Property
+	 * @var bool
+	 */
+	public $useI18nForDest;
+	
+	public function __construct($sourceFormat=null, $destFormat=null, $useI18nForDest=null) {
+		$this->sourceFormat = $sourceFormat;
+		$this->destFormat = $destFormat;
+		$this->useI18nForDest = $useI18nForDest;
+	}
+
+	/**
+	 * Returns the real dest format (using translation if necessary). 
+	 *
+	 * @return string
+	 */
+	public function getDestFormat() {
+		if ($this->useI18nForDest) {
+			return iMsg($this->destFormat);
+		} else {
+			return $this->destFormat;
+		}
+	}
+	
+	/**
+	 * Formats the value.
+	 *
+	 * @param string $value
+	 */
+	public function format($value) {
+    	if ($this->sourceFormat == "timestamp") {
+    		return date($destFormat, $value);
+    	} else {
+    		return DateTime::createFromFormat($sourceFormat, $value)->format($destFormat);
+    	}
+		
+	}
+
+}
+?>
