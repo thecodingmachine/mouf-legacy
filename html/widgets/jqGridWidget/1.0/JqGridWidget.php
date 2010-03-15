054cda5f0df179cfe8874c3c41ec3b2503002d39
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2010-03-15 16:10:04 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@214 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- html/widgets/jqGridWidget/1.0/JqGridWidget.php
+++ html/widgets/jqGridWidget/1.0/JqGridWidget.php
@@ -127,7 +127,7 @@ jQuery(document).ready(function(){';
     pager: '#".$pagerId."',
     height:$this->height,
     rowNum:$this->rowNum,
-    rowList:[10,20,30,100],
+    rowList:[10,20,30,50,100],
     sortname: '".$this->defaultSortColumn."',
     sortorder: '".$this->defaultSortOrder."',
     viewrecords: true,";
