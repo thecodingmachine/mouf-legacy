d3fafe2ed9692c7fa890ee8695a7825012dfc07f
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2010-03-24 10:11:17 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@217 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- index.php
+++ index.php
@@ -29,6 +29,10 @@ if (!extension_loaded("curl")) {
 		<p>Please click the install button below. This will create and install a ".htaccess" file in the "Mouf" directory.
 		This will also create 5 files in your root directory: Mouf.php, MoufComponents.php, MoufRequire.php, MoufUI.php and MoufUniversalParameters.php (if they don't already exist)</p>
 		<p>Please make sure that the Mouf directory is writable by your web-server.</p>
+		<p>Finally, please make sure that the Apache Rewrite module is enabled on your server. Since this install process will create a ".htaccess" file, 
+		you must make sure it will be taken into account. If after clicking the "Install" button, nothing happens, it is likely that your Apache server
+		has been configured to ignore the ".htaccess" files. In this case, please dive into your Apache configuration and look for a "<code>AllowOverride</code>" directive.
+		You should set this directive to: "<code>AllowOverride All</code>".</p>
 		
 		<form action="install.php" method="post">
 			<input type="submit" value="Install" />
