605f7d471d6b741d701d1bb59b8c37c8d1de8143
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2010-04-13 17:03:31 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@223 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- MoufInstanceNotFoundException.php
+++ MoufInstanceNotFoundException.php
@@ -6,5 +6,25 @@
  */
 class MoufInstanceNotFoundException extends MoufException {
 
+	/**
+	 * The name of the instance that was not found.
+	 *
+	 * @var string
+	 */
+	private $instanceName;
+	
+	public function __construct($msg, $code, $instanceName) {
+		parent::__construct($msg, $code);
+		$this->instanceName = $instanceName;
+	}
+	
+	/**
+	 * Returns the name of the instance that was not found.
+	 *
+	 * @return string
+	 */
+	public function getMissingInstanceName() {
+		return $this->instanceName;
+	}
 }
 ?>
