5d2636daf36b67564db22402781683de339d1af6
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2010-07-19 08:48:25 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@286 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- javascript/jquery/jquery-validationEngine/1.6.3/HtmlJSJQueryValidateEngine.php
+++ javascript/jquery/jquery-validationEngine/1.6.3/HtmlJSJQueryValidateEngine.php
@@ -11,14 +11,14 @@ class HtmlJSJQueryValidateEngine implements HtmlElementInterface {
 	public function toHtml() {
 		global $i18n_lg;
 		
-		echo '<link rel="stylesheet" type="text/css" media="screen" href="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationengine/1.6.3/css/validationEngine.jquery.css" media="screen" />'."\n";
+		echo '<link rel="stylesheet" type="text/css" media="screen" href="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationEngine/1.6.3/css/validationEngine.jquery.css" media="screen" />'."\n";
 		
 		if ($i18n_lg == "fr") {
-			echo '<script type="text/javascript" src="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationengine/1.6.3/js/jquery.validationEngine-fr.js"></script>'."\n";
+			echo '<script type="text/javascript" src="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationEngine/1.6.3/js/jquery.validationEngine-fr.js"></script>'."\n";
 		} else {
-			echo '<script type="text/javascript" src="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationengine/1.6.3/js/jquery.validationEngine-en.js"></script>'."\n";
+			echo '<script type="text/javascript" src="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationEngine/1.6.3/js/jquery.validationEngine-en.js"></script>'."\n";
 		}
-		echo '<script type="text/javascript" src="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationengine/1.6.3/js/jquery.validationEngine.js"></script>'."\n";
+		echo '<script type="text/javascript" src="'.ROOT_URL.'plugins/javascript/jquery/jquery-validationEngine/1.6.3/js/jquery.validationEngine.js"></script>'."\n";
 	}
 }
 
