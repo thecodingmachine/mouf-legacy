ee53675ca5036f1cd9ee4b06fb024229059fd69c
marc <marc@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2010-12-06 16:09:47 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@372 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ utils/i18n/fine/2.0/ressources/message.php
@@ -0,0 +1,14 @@
+<?php
+$msg['date.format.php.short']="mm/dd/yy";
+$msg['month.1']="january";
+$msg['month.2']="febuary";
+$msg['month.3']="march";
+$msg['month.4']="april";
+$msg['month.5']="may";
+$msg['month.6']="june";
+$msg['month.7']="july";
+$msg['month.8']="august";
+$msg['month.9']="september";
+$msg['month.10']="october";
+$msg['month.11']="november";
+$msg['month.12']="december";
