47acbcabeec863854b62f5921b26f4819612e05e
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2010-12-09 18:21:27 +0000

    Commiting a bridge between FINE and Drupal language detection.
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@382 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ integration/drupal/drupal_fine_bridge/1.0/DrupalLanguageDetection.php
@@ -0,0 +1,39 @@
+<?php 
+
+/**
+ * The DrupalLanguageDetection class returns the language detected by Drupal.
+ * Of course, Drupal must be installed, and bootstraped.
+ * The easiest way to do this is to use the Druplash module.
+ * 
+ * @author David Negrier
+ * @Component
+ */
+class DrupalLanguageDetection implements LanguageDetectionInterface {
+	
+	/**
+	 * While FINE as a notion if "default" language, Drupal as not.
+	 * Instead, the default language for Drupal is always "English".
+	 * By ticking this box, if Drupal returns "english", this class
+	 * will return "default" instead.
+	 * 
+	 * @Property
+	 * @var boolean
+	 */
+	public $englishAsDefault;
+	
+	/**
+	 * Returns the language used in Drupal.
+	 * 
+	 * @see plugins/utils/i18n/fine/2.0/language/LanguageDetectionInterface::getLanguage()
+	 * @return string
+	 */
+	public function getLanguage() {
+		global $language;
+		$lang = $language->language;
+		if ($this->englishAsDefault && $lang=="en") {
+			$lang = "default";
+		}
+		return $lang;
+	}
+
+}
