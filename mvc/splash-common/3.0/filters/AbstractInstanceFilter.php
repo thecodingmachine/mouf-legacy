a2b385573102b33703e8338566cf36cfa13dbcc9
knguyen <knguyen@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2010-12-22 15:40:24 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@407 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ mvc/splash-common/3.0/filters/AbstractInstanceFilter.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class to be extended to add a filter to an action.
+ * Those filters, unlike AbsrtactFilters, must be instanciated as a Mouf instance.
+ */
+abstract class AbstractInstanceFilter extends AbstractFilter
+{
+	/**
+	 * Sets the parameters stored in this annotation.
+	 * This function is automatically called just after the annotation is created.
+	 * 
+	 * @param string $value
+	 */
+	abstract function setValue($value);	
+}
+?>
