589ccad7b0c14ce1ec4fdaa1fbbccde974ae97b9
marc <marc@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2011-03-29 13:36:37 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@514 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- utils/i18n/fine/2.1/FineExternalComponents.php
+++ utils/i18n/fine/2.1/FineExternalComponents.php
@@ -1,3 +1,3 @@
 <?php
 MoufManager::getMoufManager()->declareComponent('fineCommonTranslationService', 'FinePHPArrayTranslationService', true);
-MoufManager::getMoufManager()->setParameter('fineCommonTranslationService', 'i18nMessagePath', 'plugins/utils/i18n/fine/2.1/ressources/');
+MoufManager::getMoufManager()->setParameter('fineCommonTranslationService', 'i18nMessagePath', 'plugins/utils/i18n/fine/2.1/resources/');
