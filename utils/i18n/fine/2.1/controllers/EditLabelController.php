c790f72b2da4d0cc9a33d70e969d35c5500264d0
marc <marc@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2011-04-28 13:22:59 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@536 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- utils/i18n/fine/2.1/controllers/EditLabelController.php
+++ utils/i18n/fine/2.1/controllers/EditLabelController.php
@@ -418,9 +418,9 @@ class EditLabelController extends Controller implements MoufSearchable {
 	 */
 	protected static function setTranslationForMessageFromService($selfEdit, $msgInstanceName, $key, $label, $language, $delete) {
 
-		$url = MoufReflectionProxy::getLocalUrlToProject()."plugins/utils/i18n/fine/2.1/direct/set_message_translation.php?msginstancename=".urlencode($msgInstanceName)."&selfedit=".(($selfEdit)?"true":"false")."&key=".urlencode($key)."&label=".urlencode($label)."&language=".urlencode($language)."&delete=".urlencode($delete);
+		$url = MoufReflectionProxy::getLocalUrlToProject()."plugins/utils/i18n/fine/2.1/direct/set_message_translation.php?msginstancename=".urlencode($msgInstanceName)."&selfedit=".(($selfEdit)?"true":"false")."&key=".urlencode($key)."&language=".urlencode($language)."&delete=".urlencode($delete);
 		 
-		$response = self::performRequest($url);
+		$response = self::performRequest($url, array("label" => urlencode($label)));
 
 		$obj = unserialize($response);
 		
