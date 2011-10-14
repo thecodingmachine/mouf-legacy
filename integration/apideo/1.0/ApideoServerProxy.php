56195aeb107e9558ea939527339f101fa5fbd05f
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2011-10-14 13:06:44 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@672 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- integration/apideo/1.0/ApideoServerProxy.php
+++ integration/apideo/1.0/ApideoServerProxy.php
@@ -22,13 +22,13 @@ class ApideoServerProxy {
 	 * @param string $apideoKey
 	 * @param string $securityPhrase
 	 * @param string $script
-	 * @throws ApplicationException
+	 * @throws Exception
 	 */
 	public function uploadScript($apideoKey, $securityPhrase, $script) {
-		$url = VIDEOSERVER_URL."uploadScript.do";
+		$url = $this->videoServerURL."uploadScript.do";
 		
 		// Note: the adminsecurityphrase is stored directly in the program below, and in the Java code on the videoserver side.
-		$params = array("apideokey"=>urlencode($apideoKey), "securityPhrase"=>urlencode($securityPhrase), "script"=>urlencode($script));
+		$params = array("apideokey"=>urlencode($apideoKey), "securityphrase"=>urlencode($securityPhrase), "script"=>urlencode($script));
 		
 		$fields_string = "";
 		foreach($params as $key=>$value) { $fields_string  .= $key.'='.$value.'&'; }
@@ -44,7 +44,7 @@ class ApideoServerProxy {
 		curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields_string );
 		
 		if( curl_error($ch) ) { 
-			throw new ApplicationException("An error occured: ".curl_error($ch));
+			throw new Exception("An error occured: ".curl_error($ch));
 		} else {
 			$response = curl_exec( $ch );
 		}
@@ -54,12 +54,12 @@ class ApideoServerProxy {
 		$xmlRoot = simplexml_load_string($response);
 		
 		if ($xmlRoot == null) {
-			throw new ApplicationException("An error occured while relocating room '".$roomName."' for Apideo key: ".$apideoKey, $response);
+			throw new Exception("An error occured while uploading script for Apideo key: ".$apideoKey.". Response: ".$response);
 		}
 		
 		// If an error message is returned instead of a message
 		if (!empty($xmlRoot->message)) {
-			throw new ApplicationException("An error occured while relocating room '".$roomName."' for Apideo key: ".$apideoKey.". ".$xmlRoot->message." - ".$xmlRoot->stacktrace);
+			throw new Exception("An error occured while uploading script for Apideo key: ".$apideoKey.". ".$xmlRoot->message." - ".$xmlRoot->stacktrace);
 		}
 	}
 	
@@ -68,13 +68,13 @@ class ApideoServerProxy {
 	 *
 	 * @param string $apideoKey
 	 * @param string $securityPhrase
-	 * @throws ApplicationException
+	 * @throws Exception
 	 */
 	public function getScript($apideoKey, $securityPhrase) {
-		$url = VIDEOSERVER_URL."getScript.do";
+		$url = $this->videoServerURL."getScript.do";
 	
 		// Note: the adminsecurityphrase is stored directly in the program below, and in the Java code on the videoserver side.
-		$params = array("apideokey"=>urlencode($apideoKey), "securityPhrase"=>urlencode($securityPhrase));
+		$params = array("apideokey"=>urlencode($apideoKey), "securityphrase"=>urlencode($securityPhrase));
 	
 		$fields_string = "";
 		foreach($params as $key=>$value) {
@@ -92,7 +92,7 @@ class ApideoServerProxy {
 		curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields_string );
 	
 		if( curl_error($ch) ) {
-			throw new ApplicationException("An error occured: ".curl_error($ch));
+			throw new Exception("An error occured: ".curl_error($ch));
 		} else {
 			$response = curl_exec( $ch );
 		}
@@ -102,12 +102,12 @@ class ApideoServerProxy {
 		$xmlRoot = simplexml_load_string($response);
 	
 		if ($xmlRoot == null) {
-			throw new ApplicationException("An error occured while relocating room '".$roomName."' for Apideo key: ".$apideoKey, $response);
+			throw new Exception("An error occured while retrieving server-side script for Apideo key: ".$apideoKey.". Response: ".$response);
 		}
 	
 		// If an error message is returned instead of a message
 		if (!empty($xmlRoot->message)) {
-			throw new ApplicationException("An error occured while relocating room '".$roomName."' for Apideo key: ".$apideoKey.". ".$xmlRoot->message." - ".$xmlRoot->stacktrace);
+			throw new Exception("An error occured while retrieving server-side script for Apideo key: ".$apideoKey.". ".$xmlRoot->message." - ".$xmlRoot->stacktrace);
 		}
 		
 		return (string) $xmlRoot->serversidescript;
