3fb04ae0c6babe521b3918c10694681d583ec7de
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-05-10 15:27:15 +0000

    Removing unseen dependency on Splash
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@838 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- utils/log/errorlog_logger/1.1/ErrorLogLogger.php
+++ utils/log/errorlog_logger/1.1/ErrorLogLogger.php
@@ -72,13 +72,109 @@ class ErrorLogLogger implements LogInterface {
 				$trace = debug_backtrace();
 				error_log($level.': '.$trace[1]['file']."(".$trace[1]['line'].") ".(isset($trace[2])?($trace[2]['class'].$trace[2]['type'].$trace[2]['function']):"")." -> ".$string);
 			} else {
-				error_log($level.': '.ExceptionUtils::getTextForException($string));
+				error_log($level.': '.self::getTextForException($string));
 			}
 		} else {
 			$trace = debug_backtrace();
-			error_log($level.': '.$trace[1]['file']."(".$trace[1]['line'].") ".(isset($trace[2])?($trace[2]['class'].$trace[2]['type'].$trace[2]['function']):"")." -> ".$string."\n".ExceptionUtils::getTextForException($e));
+			error_log($level.': '.$trace[1]['file']."(".$trace[1]['line'].") ".(isset($trace[2])?($trace[2]['class'].$trace[2]['type'].$trace[2]['function']):"")." -> ".$string."\n".self::getTextForException($e));
 		}
 
+	}
+	
+	
+	/**
+	 * Function called to display an exception if it occurs.
+	 * It will make sure to purge anything in the buffer before calling the exception displayer.
+	 *
+	 * @param Exception $exception
+	 */
+	static function getTextForException(Exception $exception) {
+		// Now, let's compute the same message, but without the HTML markup for the error log.
+		$textTrace = "Message: ".$exception->getMessage()."\n";
+		$textTrace .= "File: ".$exception->getFile()."\n";
+		$textTrace .= "Line: ".$exception->getLine()."\n";
+		$textTrace .= "Stacktrace:\n";
+		$textTrace .= self::getTextBackTrace($exception->getTrace());
+		return $textTrace;
+	}
+	
+	/**
+	 * Returns the Exception Backtrace as a text string.
+	 *
+	 * @param unknown_type $backtrace
+	 * @return unknown
+	 */
+	static private function getTextBackTrace($backtrace) {
+		$str = '';
+	
+		foreach ($backtrace as $step) {
+			if ($step['function']!='getTextBackTrace' && $step['function']!='handle_error')
+			{
+				if (isset($step['file']) && isset($step['line'])) {
+					$str .= "In ".$step['file'] . " at line ".$step['line'].": ";
+				}
+				if (isset($step['class']) && isset($step['type']) && isset($step['function'])) {
+					$str .= $step['class'].$step['type'].$step['function'].'(';
+				}
+	
+				if (is_array($step['args'])) {
+					$drawn = false;
+					$params = '';
+					foreach ( $step['args'] as $param)
+					{
+						$params .= self::getPhpVariableAsText($param);
+						//$params .= var_export($param, true);
+						$params .= ', ';
+						$drawn = true;
+					}
+					$str .= $params;
+					if ($drawn == true)
+					$str = substr($str, 0, strlen($str)-2);
+				}
+				$str .= ')';
+				$str .= "\n";
+			}
+		}
+	
+		return $str;
+	}
+	
+	/**
+	 * Used by the debug function to display a nice view of the parameters.
+	 *
+	 * @param unknown_type $var
+	 * @return unknown
+	 */
+	private static function getPhpVariableAsText($var) {
+		if( is_string( $var ) )
+		return( '"'.str_replace( array("\x00", "\x0a", "\x0d", "\x1a", "\x09"), array('\0', '\n', '\r', '\Z', '\t'), $var ).'"' );
+		else if( is_int( $var ) || is_float( $var ) )
+		{
+			return( $var );
+		}
+		else if( is_bool( $var ) )
+		{
+			if( $var )
+			return( 'true' );
+			else
+			return( 'false' );
+		}
+		else if( is_array( $var ) )
+		{
+			$result = 'array( ';
+			$comma = '';
+			foreach( $var as $key => $val )
+			{
+				$result .= $comma.self::getPhpVariableAsText( $key ).' => '.self::getPhpVariableAsText( $val );
+				$comma = ', ';
+			}
+			$result .= ' )';
+			return( $result );
+		}
+	
+		elseif (is_object($var)) return "Object ".get_class($var);
+		elseif(is_resource($var)) return "Resource ".get_resource_type($var);
+		return "Unknown type variable";
 	}
 }
 
