3def39ddc057c15326a5659f47126dda4a6d9fe0
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-06-04 13:56:44 +0000

    Improving resize filter
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@853 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- utils/graphics/moufimage/1.0/filters/MoufImageResize.php
+++ utils/graphics/moufimage/1.0/filters/MoufImageResize.php
@@ -49,6 +49,49 @@ class MoufImageResize implements MoufImageInterface{
 	public $allowEnlarge = false;
 	
 	/**
+	 * Enforce the requested dimensions.
+	 * This applies only if $keepRatio == true, and if the requested ratio is not equal to the source ratio.
+	 * If $enforceRequestedDimensions == true, the destination image will occupy only a portion of the generated image.
+	 * The rest of the image will be a background color that can be set via the $backgroundColor property.
+	 * 
+	 * @Property
+	 * @var bool
+	 */
+	public $enforceRequestedDimensions = false;
+	
+	/**
+	 * The red part of the background color (if any)
+	 * 
+	 * @Property
+	 * @var int
+	 */
+	public $backgroundRed = 255;
+	
+	/**
+	 * The green part of the background color (if any)
+	 * 
+	 * @Property
+	 * @var int
+	 */
+	public $backgroundGreen = 255;
+	
+	/**
+	 * The blue part of the background color (if any)
+	 * 
+	 * @Property
+	 * @var int
+	 */
+	public $backgroundBlue = 255;
+	
+	/**
+	 * The alpha channel of the background color (if any)
+	 * 
+	 * @Property
+	 * @var int
+	 */
+	public $backgroundAlpha = 127;
+	
+	/**
 	 * Get the GD Image resource after effect has been applied
 	 * @return $image, the GD resource image
 	 */
@@ -65,6 +108,8 @@ class MoufImageResize implements MoufImageInterface{
 			//do Nothing : the image is smaller than the target rectangle, so it should remain unchanged
 			$newWidth = $oWidth;
 			$newHeight = $oHeight;
+			$newImageWidth = $oWidth;
+			$newImageHeight = $oHeight;
 		}else if ($this->keepRatio){
 			//image doesn't fit the target rectangle (at least on dimension is greater), the final ratio is the one that make the resized image fit the target rectangle
 			$xRation = (float) $oWidth / $this->width;
@@ -74,26 +119,33 @@ class MoufImageResize implements MoufImageInterface{
 			
 			$newWidth = $oWidth / $finalRatio;
 			$newHeight = $oHeight / $finalRatio;
-				
+
+			if (!$this->enforceRequestedDimensions) {
+				$newImageWidth = $newWidth;
+				$newImageHeight = $newHeight;
+			} else {
+				$newImageWidth = $this->width;
+				$newImageHeight = $this->height;
+			}
 		}else{
 			//Simply apply a stupid resize, whater the original image's ratio is
 			$newWidth = $this->width;
 			$newHeight = $this->height;
+			$newImageWidth = $newWidth;
+			$newImageHeight = $newHeight;
 		}
 		
-		
-		
-		$new_image = imagecreatetruecolor($newWidth, $newHeight);
+		$new_image = imagecreatetruecolor($newImageWidth, $newImageHeight);
 		
 		//If image id of type PNG or GIF, presenve Transprency
 		if(($imgInfo[2] == 1) || ($imgInfo[2]==3)){
 			imagealphablending($new_image, false);
 			imagesavealpha($new_image,true);
-			$transparent = imagecolorallocatealpha($new_image, 255, 255, 255, 127);
-			imagefilledrectangle($new_image, 0, 0, $newWidth, $newHeight, $transparent);
 		}
+		$transparent = imagecolorallocatealpha($new_image, $this->backgroundRed, $this->backgroundGreen, $this->backgroundBlue, $this->backgroundAlpha);
+		imagefilledrectangle($new_image, 0, 0, $newImageWidth, $newImageHeight, $transparent);
 		
-		imagecopyresampled($new_image, $imageResource, 0, 0, 0, 0, $newWidth, $newHeight, $oWidth, $oHeight);
+		imagecopyresampled($new_image, $imageResource, ($newImageWidth - $newWidth)/2, ($newImageHeight - $newHeight)/2, 0, 0, $newWidth, $newHeight, $oWidth, $oHeight);
 		
 		$moufImageResource->resource = $new_image;
 		
