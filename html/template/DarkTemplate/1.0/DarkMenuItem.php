90b02d9927d113cce43effac109f6d9a8383db61
rem <rem@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-06-08 08:49:42 +0000

    add the DarkTemplate
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@854 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ html/template/DarkTemplate/1.0/DarkMenuItem.php
@@ -0,0 +1,134 @@
+<?php
+/*
+ * Copyright (c) 2012 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+
+
+/**
+ * This class represent a menu item (a link)
+ *
+ * @Component
+ */
+class DarkMenuItem implements HtmlElementInterface {
+	
+	/**
+	 * The text for the menu item
+	 *
+	 * @Property
+	 * @Compulsory
+	 * @var string
+	 */
+	public $menuText;
+	
+	/**
+	 * The link for the menu (relative to the root url), unless it starts with / or http:// or https://.
+	 *
+	 * @Property
+	 * @var string
+	 */
+	public $menuLink;
+	
+	/**
+	 * The CSS class for the menu, if any (applied to the &lt;li&gt; element.
+	 *
+	 * @Property
+	 * @var string
+	 */
+	public $menuCssClass;
+	
+	/**
+	 * A list of parameters that are propagated by the link.
+	 * For instance, if the parameter "mode" is set to 42 on the page (because the URL is http://mywebsite/myurl?mode=42),
+	 * then if you choose to propagate the "mode" parameter, the menu link will have "mode=42" as a parameter.
+	 *
+	 * @Property
+ 	 * @var array<string>
+	 */
+	public $propagatedUrlParameters;
+	
+	/**
+	 * This condition must be matched to display the menu.
+	 * Otherwise, the menu is not displayed.
+	 * The displayCondition is optional. If no condition is set, the menu will always be displayed. 
+	 *
+	 * @Property
+	 * @var ConditionInterface
+	 */
+	public $displayCondition;
+	
+	/**
+	 * Constructor.
+	 *
+	 * @param string $menuText
+	 * @param string $menuLink
+	 * @param string $menuCssClass
+	 * @param array<string> $propagatedUrlParameters
+	 */
+	public function __construct($menuText=null, $menuLink=null, $menuCssClass=null, $propagatedUrlParameters=null) {
+		$this->menuText = $menuText;
+		$this->menuLink = $menuLink;
+		$this->menuCssClass = $menuCssClass;
+		$this->propagatedUrlParameters = $propagatedUrlParameters;
+	}
+	
+	public function toHtml() {
+		if ($this->displayCondition == null || $this->displayCondition->isOk($this)) {
+			echo '<li ';
+			if (!empty($this->menuCssClass)) {
+				echo 'class="'.$this->menuCssClass.'"';
+			}
+			echo '>';
+			if (!empty($this->menuLink)) {
+				echo '<a href="'.$this->getLinkWithParams().'" >';
+			}
+			echo $this->menuText;
+			if (!empty($this->menuLink)) {
+				echo '</a>';
+			}
+			echo '</li>';
+		}
+	}
+
+	private function getLinkWithParams() {
+		$link = $this->getLink();
+		
+		$params = array();
+		// First, get the list of all parameters to be propagated
+		if (is_array($this->propagatedUrlParameters)) {
+			foreach ($this->propagatedUrlParameters as $parameter) {
+				if (isset($_REQUEST[$parameter])) {
+					$params[$parameter] = get($parameter);
+				}
+			}
+		}
+		
+		if (!empty($params)) {
+			if (strpos($link, "?") === FALSE) {
+				$link .= "?";
+			} else {
+				$link .= "&";
+			}
+			$paramsAsStrArray = array();
+			foreach ($params as $key=>$value) {
+				$paramsAsStrArray[] = urlencode($key).'='.urlencode($value);
+			}
+			$link .= implode("&", $paramsAsStrArray);
+		}
+		
+		return $link;
+	}
+	
+	private function getLink() {
+		if (strpos($this->menuLink, "/") === 0
+			|| strpos($this->menuLink, "http://") === 0
+			|| strpos($this->menuLink, "https://") === 0) {
+			return $this->menuLink;	
+		}
+		
+		return ROOT_URL.$this->menuLink;
+	}
+	
+}
+?>
---
+++ html/template/DarkTemplate/1.0/views/dark.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<?php if ($this->favIconUrl) { ?>
+		<link rel="icon" type="image/png" href="<?php echo ROOT_URL.$this->favIconUrl; ?>" />
+		<?php } ?>
+		<title><?php print $this->title ?></title>
+		<?php print $this->getCssFiles() ?>
+		<?php $this->drawArray($this->head); ?>
+		<?php print $this->getJsFiles() ?>
+		
+	</head>
+
+<!-- default margin = default layout -->
+<body>
+
+<div class="container">
+
+	<div id="header" class="header">
+				<div id="logo">
+					<a href="<?php echo ROOT_URL ?>">
+						<span><?php print $this->title ?></span>
+					</a>
+				</div>
+	</div> 
+
+	<div class="stripes"><span></span></div>
+	
+			
+				
+	
+	<?php if (count($this->header) != 0) { ?>
+	<div class="nav">
+		<?php $this->drawArray($this->header); ?>
+		<div class="clearer"><span></span></div>
+	</div>
+		<?php } ?>
+	<div class="stripes"><span></span></div>
+
+	<div class="main">
+	
+	<?php if (count($this->left) != 0) { ?>
+				<div id="sidebar-left" class="left">
+					<?php $this->drawArray($this->left);?>
+				</div>
+			<?php } ?>
+	
+	
+
+			
+		<?php /*if (count($this->right) != 0) { ?>
+				<div id="sidebar-right" class="right">
+					<?php $this->drawArray($this->right);?>
+				</div>
+			<?php }*/ ?>
+     <div id="content" class="right">
+
+				<?php $this->drawArray($this->content); ?>
+			</div>
+
+		<div class="clearer"><span></span></div>
+
+	</div>
+	
+	
+	<?php if (count($this->footer) != 0) { ?>
+				<div class="footer" id="footer" >
+						<?php $this->drawArray($this->right);?>
+				</div>
+			<?php } ?>
+
+						
+			<div class="bottom">
+				
+				
+
+				<div class="clearer"><span></span></div>
+
+			</div>
+
+	</div>
+
+</div>
+
+</body>
+
+</html>
+
