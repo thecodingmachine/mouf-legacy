5ddd75fc25861b3c540c6802a579200048493b55
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-06-11 16:43:55 +0000

    Modifying formatters to remove the NumberFormatter class that is now included in the PHP language. Replaced by MoufNumberFormatter.
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@859 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ database/datasource/formatters/1.1/AndFormatter.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The formatter contains several formatters and applies those formatters in order.
+ *
+ * @Component
+ */
+class AndFormatter implements FormatterInterface {
+
+	/**
+	 * The formatters to apply.
+	 * 
+	 * @Property
+	 * @Compulsory
+	 * @var array<FormatterInterface>
+	 */
+	public $formatters;
+		
+	public function __construct($formatters=null) {
+		$this->formatters = $formatters;
+	}
+
+	/**
+	 * Formats the value.
+	 *
+	 * @param string $value
+	 */
+	public function format($value) {
+		foreach ($this->formatters as $formatter) {
+			$value = $formatter->format($value);
+		}
+		return $value;
+	}
+
+}
+?>
