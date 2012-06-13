dd7627ca1e637306bbf1dc25f9b4ec22a7777dd9
knguyen <knguyen@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-06-13 10:49:18 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@865 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

--- utils/graphics/moufimage/1.0/direct/displayImage.php
+++ utils/graphics/moufimage/1.0/direct/displayImage.php
@@ -4,6 +4,8 @@ require_once dirname(__FILE__).'/../../../../../../Mouf.php';
 $diplayerInstance = $_GET['instance'];
 $sourceFileName = $_GET['url'];
 
+error_log("file not found :: $sourceFileName");
+
 $imageDisplay = MoufManager::getMoufManager()->getInstance($diplayerInstance);
 
 $imageDisplay->sourceFileName = $sourceFileName;
