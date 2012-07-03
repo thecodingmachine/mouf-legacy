51a4eacbbbdd0e6132f41ee43a22831a55410d2a
david_negrier <david_negrier@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-07-03 18:32:55 +0000

    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@894 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ utils/common/validators/2.0/ValidatorUtils.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * Class providing utility methods (especially regarding translation) to the validators.
+ * 
+ * @author David Negrier
+ */
+class ValidatorUtils {
+	
+	/**
+	 * 
+	 * @param string $msg
+	 */
+	public static function translate($msg) {
+		$translationService = MoufManager::getMoufManager()->getInstance("validatorsTranslateService");
+		/* @var $translationService FinePHPArrayTranslationService */
+		
+		return call_user_func_array(array($translationService, "getTranslation"), func_get_args());
+	}
+}
+?>
