225cc70e050a013cf5662f350d91173971ffd75c
n_peguin <n_peguin@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-07-25 12:06:58 +0000

    Modify the jQueryValidationEngine plugin to become a web library.
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@925 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ javascript/jquery/jquery-validationEngine/2.1.1/install.php
@@ -0,0 +1,40 @@
+<?php
+// First, let's request the install utilities
+require_once '../../../../../mouf/actions/InstallUtils.php';
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+
+if ($moufManager->instanceExists("jQueryValidationEngineLibrary")) {
+	$jQueryValidationEngineLibrary = $moufManager->getInstanceDescriptor("jQueryValidationEngineLibrary");
+} else {
+	$jQueryValidationEngineLibrary = $moufManager->createInstance("WebLibrary");
+	$jQueryValidationEngineLibrary->setName("jQueryValidationEngineLibrary");
+}
+
+$jQueryValidationEngineLibrary->getProperty("jsFiles")->setValue(array(
+	'plugins/javascript/jquery/jquery-validationEngine/2.1.1/js/jquery.validationEngine.js'
+));
+$jQueryValidationEngineLibrary->getProperty("cssFiles")->setValue(array(
+	'plugins/javascript/jquery/jquery-validationEngine/2.1.1/css/validationEngine.jquery.css'
+));
+
+$renderer = $moufManager->getInstanceDescriptor('defaultWebLibraryRenderer');
+$jQueryValidationEngineLibrary->getProperty("renderer")->setValue($renderer);
+$jQueryValidationEngineLibrary->getProperty("dependencies")->setValue(array($moufManager->getInstanceDescriptor('jQueryUiLibrary')));
+
+$webLibraryManager = $moufManager->getInstanceDescriptor('defaultWebLibraryManager');
+if ($webLibraryManager) {
+	$libraries = $webLibraryManager->getProperty("webLibraries")->getValue();
+	$libraries[] = $jQueryValidationEngineLibrary;
+	$webLibraryManager->getProperty("webLibraries")->setValue($libraries);
+}
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
