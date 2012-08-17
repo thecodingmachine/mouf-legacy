ab0f4ebe103c2668123d9bf99c6aca8411fac7ff
vaidiep <vaidiep@20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44>
2012-08-17 10:10:17 +0000

    Add headers on a query
    git-svn-id: svn+ssh://188.165.179.87/var/svn/mouf/trunk@947 20a9f3e9-cf9c-4ed3-b8ae-ea11efa85a44

---
+++ utils/network/http/curlhtmlbrowser/1.1/CurlHTMLBrowser500Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * An exception triggerred if we try to access an HTTP page and a 500 error is returned.
+ *
+ */
+class CurlHTMLBrowser500Exception extends CurlHTMLBrowserException {
+	
+}
+?>
